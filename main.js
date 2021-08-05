0713f7baa90d8379a6fee4c824b282c51ec94aa6
xDave12x <77081815+xDave12x@users.noreply.github.com>
2021-08-05 18:44:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -25,3 +25,39 @@ function speak(){
 var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
 synth.speak(utterThis);
 }
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+if(error){
+    console.error(error);
+}
+else
+{
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML=results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+    prediction_1=results[0].label;
+    prediction_2=results[1].label;
+speak();
+if(results[0].label=="amazing"){
+    document.getElementById("update_emoji").innerHTML="&#128076;";
+}
+if(results[0].label=="best"){
+    document.getElementById("update_emoji").innerHTML="&#128077;";
+}
+if(results[0].label=="victory"){
+    document.getElementById("update_emoji").innerHTML="&#9996;";
+}
+if(results[1].label=="amazing"){
+    document.getElementById("update_emoji2").innerHTML="&#128076;";
+}
+if(results[1].label=="best"){
+    document.getElementById("update_emoji2").innerHTML="&#128077;";
+}
+if(results[1].label=="victory"){
+    document.getElementById("update_emoji2").innerHTML="&#9996;";
+}
+}
+}
